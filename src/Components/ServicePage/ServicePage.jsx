651f76454df64f6098e98005604d325a435d4dcc
Robert Kovach <kovachrobert11995@gmail.com>
2025-02-11 14:31:39 +0100

    feature create ServicePage page

--- src/Components/ServicePage/ServicePage.jsx
+++ src/Components/ServicePage/ServicePage.jsx
@@ -7,8 +7,12 @@ import { useLanguage } from "../../Components/LanguageContext/LanguageContext";
 import servicesData from "../../data/packages.json";
 import { useParams } from "react-router-dom";
 import translations from "../../data/translations";
+import BackButton from "../../Components/BackButton/BackButton";
+import productsData from "../../data/products.json";
+import UniversalSlider from "../UniversalSlider/UniversalSlider";
 
 export default function ServicePage() {
+  const product = productsData.find((p) => p.id === 1);
   const { servicesId } = useParams();
   const { language } = useLanguage();
   const texts = translations[language];
@@ -27,15 +31,37 @@ export default function ServicePage() {
       <ResponsiveMove />
       <section className="service-details">
         <div className="service-details__wrapper">
-          <h2 className="service-details__title">{texts[serviceData.title]}</h2>
-          <img
-            src={serviceData.image}
-            alt={texts[serviceData.title]}
-            className="service-details__image"
+          <BackButton />
+          <div className="service-details__info">
+            <img
+              src={`${process.env.PUBLIC_URL}${serviceData.image}`}
+              alt={texts[serviceData.title]}
+              className="service-details__image"
+            />
+            <div className="service-details__text">
+              <h2 className="service-details__title">
+                {texts[serviceData.title]}
+              </h2>
+              <p className="service-details__description">
+                {texts[serviceData.description_full]}
+              </p>
+              <button className="service-details__button buy-button">
+                {translations[language].buy_now}
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="service-comments">
+        <div className="service-comments__wrapper">
+          <div className="service-comments__title title">
+            {texts["happyCustomersSays"] || "Happy Customers Says"}
+          </div>
+          <UniversalSlider
+            items={product.comments}
+            slidesToShow={2}
+            autoplay={true}
           />
-          <p className="service-details__description">
-            {texts[serviceData.description]}
-          </p>
         </div>
       </section>
       <Footer />
