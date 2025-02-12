98910eab558d4e0962cc05a56b3375beafa86f07
Robert Kovach <kovachrobert11995@gmail.com>
2025-02-12 15:26:10 +0100

    fix logout

--- src/Components/Authorization/LogoutButton.jsx
+++ src/Components/Authorization/LogoutButton.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useLanguage } from "../LanguageContext/LanguageContext";
 import translations from "../../data/translations.json";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { language } = useLanguage();
 
   const handleLogout = () => {
     sessionStorage.removeItem("user");
-    window.location.reload();
+    navigate("/");
+    setTimeout(() => window.location.reload(), 100);
   };
+
+  useEffect(() => {
+    const checkUser = () => {
+      const user = sessionStorage.getItem("user");
+      if (!user && location.pathname === "/tickets") {
+        navigate("/");
+      }
+    };
+
+    window.addEventListener("storage", checkUser);
+    checkUser();
+
+    return () => {
+      window.removeEventListener("storage", checkUser);
+    };
+  }, [location, navigate]);
 
   return (
     <button onClick={handleLogout} className="logout-button">
       <img
         src={`${process.env.PUBLIC_URL}/img/account/logout-icon.svg`}
-        alt="profile"
+        alt="logout"
         className="user-info__ticket"
       />
       {translations[language].logout}
